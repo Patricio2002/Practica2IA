7e303972cc2a84734addac13a8f10e1842eb20bd
Emmanuel <emmanueljc1601@gmail.com>
2025-04-07 13:53:11 -0600

    Hill Climbing corregido

--- hillClimbing.py
+++ hillClimbing.py
@@ -1,4 +1,57 @@
 import networkx as nx
+from collections import deque
+
+# Crear el grafo
+def crearGrafo():
+  G = nx.DiGraph()
+  for nodo, (imdb, duracion, anio, hijos) in nodos.items():
+    G.add_node(nodo, imdb=imdb, duracion=duracion, anio=anio)
+    for hijo in hijos:
+        G.add_edge(nodo, hijo)
+  return G
+
+#Definicion de la Heuristica
+def heuristic(G, nodo):
+  """Heurística basada en los valores de los nodos."""
+  return (G.nodes[nodo]['anio']/100) + G.nodes[nodo]['imdb'] - abs(G.nodes[nodo]['duracion'] - 120)
+
+# Implementación de Hill Climbing
+def ascenso(G, inicio, sol):
+  actual=inicio
+  recorrido = deque()
+  orden = deque()
+  orden.append (actual)
+  while True:
+    recorrido.append(actual)
+    mejor = []
+    aux = {}
+    vecinos = list(G.successors(actual))
+    if actual == sol:
+      return recorrido
+    if not vecinos:
+      if not orden:
+          return recorrido
+      else:
+          while orden:  # verifica si orden no está vacío
+              actual = orden.popleft()
+              if actual not in recorrido:
+                  break
+          else:
+              return recorrido
+    else:
+      for i in vecinos:
+        mejor.append(heuristic(G,i))
+        aux[heuristic(G,i)] = i
+      mejor.sort(reverse=True)
+      for i in mejor:
+        orden.append(aux[i])
+      actual=aux[mejor[0]]
+
+# Dibujar el grafo en la terminal
+def dibujarGrafo(G):
+    for nodo in G.nodes:
+        hijos = list(G.successors(nodo))
+        print(f"{nodo} -> {', '.join(hijos) if hijos else '∅'}")
 
 # Definir los nodos con sus conexiones
 nodos = {
@@ -7,8 +60,8 @@ nodos = {
   "El planeta de los simios": (8.0,112,1968,["Star Wars: Episodio IV","Alien"]),
   "La naranja mecanica": (8.2, 136, 1971,[]),
   "Soylent Green": (7.0,97,1973,["Blade Runner","Tron"]),
-  "Star Wars: Episodio IV": (8.6, 121,1977,["Star Wars: Episodio V","Alien"]),
-  "Alien": (8.5,117, 1979,["E.T.","Ghostbusters","The Terminator"]),
+  "Star Wars: Episodio IV": (8.6, 121,1977,["Star Wars: Episodio V","The Terminator"]),
+  "Alien": (8.5,117, 1979,["E.T.","Ghostbusters"]),
   "Star Wars: Episodio V": (8.7,124,1980,["Star Wars: Episodio VI","Volver al futuro"]),
   "E.T.": (7.9,115,1982,[]),
   "Blade Runner": (8.1,117,1982,[]),
@@ -25,42 +78,15 @@ nodos = {
   "The Matrix": (8.7,136,1999,[])
 }
 
-# Crear el grafo
-def crearGrafo():
-  G = nx.DiGraph()
-  for nodo, (imdb, duracion, anio, hijos) in nodos.items():
-    G.add_node(nodo, imdb=imdb, duracion=duracion, anio=anio)
-    for hijo in hijos:
-        G.add_edge(nodo, hijo)
-  return G
-
 grafo=crearGrafo()
-
-def heuristic(G, nodo):
-  """Heurística basada en los valores de los nodos."""
-  return (G.nodes[nodo]['anio']/100) + G.nodes[nodo]['imdb'] - abs(G.nodes[nodo]['duracion'] - 120)
-
-# Implementación de Hill Climbing
-def ascenso(G, inicio):
-  actual = inicio
-  while True:
-    vecinos = list(G.successors(actual))
-    if not vecinos:
-        return actual
-    mejor_vecino = max(vecinos, key=lambda n: heuristic(G, n))
-    if heuristic(G, mejor_vecino) <= heuristic(G, actual):
-        return actual
-    actual = mejor_vecino
+sol = "Jurassic Park" #Nodo objetivo
+# Ejecutar Hill Climbing desde el nodo 'Metropolis'
+recorrido = ascenso(grafo, "Metropolis",sol)
+if recorrido[-1] == sol:
+  for i in recorrido:
+    print(f"{i}")
+else:
+  print("No llego a la solucion")
 
 # Dibujar el grafo en la terminal
-def dibujarGrafo(G):
-    for nodo in G.nodes:
-        hijos = list(G.successors(nodo))
-        print(f"{nodo} -> {', '.join(hijos) if hijos else '∅'}")
-
-# Ejecutar Hill Climbing desde el nodo 'A'
-mejor_nodo = ascenso(grafo, "Metropolis")
-print(f"El mejor nodo encontrado es: {mejor_nodo}")
-
-# Dibujar el grafo en la terminal
-dibujarGrafo(grafo)
+#dibujarGrafo(grafo)
